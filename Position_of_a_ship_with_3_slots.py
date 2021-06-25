b723e01cd77723bc6f2513811d035c57f02ce490
Serhii Tereshchuk <57225875+ters81@users.noreply.github.com>
2021-06-25 15:11:32 +0300

    Add files via upload

---
+++ Position_of_a_ship_with_3_slots.py
@@ -0,0 +1,134 @@
+
+from random import choice, randint
+from Position_of_a_ship_with_4_slots import Position_of_a_ship_with_4_slots
+
+# 0 - empty cell
+# 1 - cell with a ship
+# 8 - forbidden cell (no other ship can stand here)
+
+field, list_of_empty_cells = Position_of_a_ship_with_4_slots()
+
+for i in range(12):
+     print(field[i])
+
+# Adding a 3-deck ship
+def Position_of_a_ship_with_3_slots():
+    direction = choice(['horizontal', 'vertical'])
+
+    # direction = 'horizontal'   # Проверка
+
+    starting_cell = choice(list_of_empty_cells).split('-')
+
+    starting_cell = '10-8'.split('-')  # Проверка
+
+    starting_cell_row = int(starting_cell[0])
+    starting_cell_column = int(starting_cell[1])
+
+    print(starting_cell_row, starting_cell_column, direction)
+
+    if direction == 'horizontal' and field[starting_cell_row].count(0) == 10:
+        if starting_cell_column in (9, 10):
+            starting_cell_column = randint(1, 8)
+
+        scc = starting_cell_column
+
+        # remove used cells from the list
+        for _ in range(3):
+            field[starting_cell_row][scc] = 1
+            list_of_empty_cells.remove(str(starting_cell_row) + '-' + str(scc))
+            scc += 1
+        if (str(starting_cell_row) + '-' + str(scc)) in list_of_empty_cells:
+            list_of_empty_cells.remove(str(starting_cell_row) + '-' + str(scc))
+
+        scc = starting_cell_column - 1
+        if (str(starting_cell_row) + '-' + str(scc)) in list_of_empty_cells:
+            list_of_empty_cells.remove(str(starting_cell_row) + '-' + str(scc))
+
+        field[starting_cell_row][starting_cell_column - 1] = 8
+        field[starting_cell_row][starting_cell_column + 3] = 8
+
+        # removal of used cells from the list if the ship is adjacent to the borders of the field
+        if 1 < starting_cell_row < 10:
+            for row in [starting_cell_row - 1, starting_cell_row + 1]:
+                scc = starting_cell_column - 1
+                for _ in range(5):
+                    field[row][scc] = 8
+                    if (str(row) + '-' + str(scc)) in list_of_empty_cells:
+                        list_of_empty_cells.remove(str(row) + '-' + str(scc))
+                    scc += 1
+        elif starting_cell_row == 1:
+            scc = starting_cell_column - 1
+            for _ in range(5):
+                field[starting_cell_row + 1][scc] = 8
+                if (str(starting_cell_row + 1) + '-' + str(scc)) in list_of_empty_cells:
+                    list_of_empty_cells.remove(str(starting_cell_row + 1) + '-' + str(scc))
+                scc += 1
+        elif starting_cell_row == 10:
+            scc = starting_cell_column - 1
+            for _ in range(5):
+                field[starting_cell_row - 1][scc] = 8
+                if (str(starting_cell_row - 1) + '-' + str(scc)) in list_of_empty_cells:
+                    list_of_empty_cells.remove(str(starting_cell_row - 1) + '-' + str(scc))
+                scc += 1
+
+    elif direction == 'vertical' and field[starting_cell_column].count(0) == 10:   # РАЗОБРАТЬСЯ С КОЛЛИЧЕСТВОМ НУЛЕЙ В КОЛОНКАХ!!!
+        # If the initial position of the ship will cause it to go out of the field.
+        if starting_cell_row in (9, 10):
+            starting_cell_row = randint(1, 8)
+
+        print(starting_cell_row, starting_cell_column, direction)
+
+        scr = starting_cell_row
+
+        # remove used cells from the list
+        for _ in range(3):
+            field[scr][starting_cell_column] = 1
+            list_of_empty_cells.remove(str(scr) + '-' + str(starting_cell_column))
+            scr += 1
+        if (str(scr) + '-' + str(starting_cell_column)) in list_of_empty_cells:
+            list_of_empty_cells.remove(str(scr) + '-' + str(starting_cell_column))
+
+        scr = starting_cell_row - 1
+        if (str(scr) + '-' + str(starting_cell_column)) in list_of_empty_cells :
+            list_of_empty_cells.remove(str(scr) + '-' + str(starting_cell_column))
+
+        field[starting_cell_row-1][starting_cell_column] = 8
+        field[starting_cell_row+3][starting_cell_column] = 8
+
+        # removal of used cells from the list if the ship is adjacent to the borders of the field
+        if 1 < starting_cell_column < 10:
+            for column in [starting_cell_column - 1, starting_cell_column + 1]:
+                scr = starting_cell_row - 1
+                for _ in range(5):
+                    field[scr][column] = 8
+                    if (str(scr) + '-' + str(column)) in list_of_empty_cells:
+                        list_of_empty_cells.remove(str(scr) + '-' + str(column))
+                    scr += 1
+        elif starting_cell_column == 1:
+            scr = starting_cell_row - 1
+            for _ in range(5):
+                field[scr][starting_cell_column + 1] = 8
+                if (str(scr) + '-' + str(starting_cell_column + 1)) in list_of_empty_cells:
+                    list_of_empty_cells.remove(str(scr) + '-' + str(starting_cell_column + 1))
+                scr += 1
+        elif starting_cell_column == 10:
+            scr = starting_cell_row - 1
+            for _ in range(5):
+                field[scr][starting_cell_column - 1] = 8
+                if (str(scr) + '-' + str(starting_cell_column - 1)) in list_of_empty_cells:
+                    list_of_empty_cells.remove(str(scr) + '-' + str(starting_cell_column - 1))
+                scr += 1
+
+
+    return list_of_empty_cells
+
+Position_of_a_ship_with_3_slots()
+
+
+
+
+for i in range(12):
+     print(field[i])
+
+print(list_of_empty_cells)
+print(len(list_of_empty_cells))
---
+++ Position_of_a_ship_with_4_slots.py
@@ -0,0 +1,144 @@
+
+from random import choice, randint
+
+# 0 - empty cell
+# 1 - cell with a ship
+# 8 - forbidden cell (no other ship can stand here)
+
+# Create empty field
+field = [
+    [8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
+    [8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8]
+        ]
+list_of_empty_cells = [
+    '1-1', '1-2', '1-3', '1-4', '1-5', '1-6', '1-7', '1-8', '1-9', '1-10',
+    '2-1', '2-2', '2-3', '2-4', '2-5', '2-6', '2-7', '2-8', '2-9', '2-10',
+    '3-1', '3-2', '3-3', '3-4', '3-5', '3-6', '3-7', '3-8', '3-9', '3-10',
+    '4-1', '4-2', '4-3', '4-4', '4-5', '4-6', '4-7', '4-8', '4-9', '4-10',
+    '5-1', '5-2', '5-3', '5-4', '5-5', '5-6', '5-7', '5-8', '5-9', '5-10',
+    '6-1', '6-2', '6-3', '6-4', '6-5', '6-6', '6-7', '6-8', '6-9', '6-10',
+    '7-1', '7-2', '7-3', '7-4', '7-5', '7-6', '7-7', '7-8', '7-9', '7-10',
+    '8-1', '8-2', '8-3', '8-4', '8-5', '8-6', '8-7', '8-8', '8-9', '8-10',
+    '9-1', '9-2', '9-3', '9-4', '9-5', '9-6', '9-7', '9-8', '9-9', '9-10',
+    '10-1', '10-2', '10-3', '10-4', '10-5', '10-6', '10-7', '10-8', '10-9', '10-10'
+]
+
+# Adding a four-deck ship
+
+def Position_of_a_ship_with_4_slots():
+    direction = choice(['horizontal', 'vertical'])
+
+    starting_cell = choice(list_of_empty_cells).split('-')
+
+    starting_cell = [3, 9]
+
+    starting_cell_row = int(starting_cell[0])
+    starting_cell_column = int(starting_cell[1])
+
+    if direction == 'horizontal':
+        # If the initial position of the ship will cause it to go out of the field.
+        if starting_cell_column in (8, 9, 10):
+            starting_cell_column = randint(1, 7)
+
+        scc = starting_cell_column
+
+        # remove used cells from the list
+        for _ in range(4):
+            field[starting_cell_row][scc] = 1
+            list_of_empty_cells.remove(str(starting_cell_row) + '-' + str(scc))
+            scc += 1
+        if (str(starting_cell_row) + '-' + str(scc)) in list_of_empty_cells :
+            list_of_empty_cells.remove(str(starting_cell_row) + '-' + str(scc))
+
+        scc = starting_cell_column - 1
+        if (str(starting_cell_row) + '-' + str(scc)) in list_of_empty_cells :
+            list_of_empty_cells.remove(str(starting_cell_row) + '-' + str(scc))
+
+        field[starting_cell_row][starting_cell_column - 1] = 8
+        field[starting_cell_row][starting_cell_column + 4] = 8
+
+        # removal of used cells from the list if the ship is adjacent to the borders of the field
+        if 1 < starting_cell_row < 10:
+            for row in [starting_cell_row - 1, starting_cell_row + 1]:
+                scc = starting_cell_column - 1
+                for _ in range(6):
+                    field[row][scc] = 8
+                    if (str(row) + '-' + str(scc)) in list_of_empty_cells:
+                        list_of_empty_cells.remove(str(row) + '-' + str(scc))
+                    scc += 1
+        elif starting_cell_row == 1:
+            scc = starting_cell_column - 1
+            for _ in range(6):
+                field[starting_cell_row + 1][scc] = 8
+                if (str(starting_cell_row + 1) + '-' + str(scc)) in list_of_empty_cells:
+                    list_of_empty_cells.remove(str(starting_cell_row + 1) + '-' + str(scc))
+                scc += 1
+        elif starting_cell_row == 10:
+            scc = starting_cell_column - 1
+            for _ in range(6):
+                field[starting_cell_row - 1][scc] = 8
+                if (str(starting_cell_row - 1) + '-' + str(scc)) in list_of_empty_cells:
+                    list_of_empty_cells.remove(str(starting_cell_row - 1) + '-' + str(scc))
+                scc += 1
+    else:
+        # If the initial position of the ship will cause it to go out of the field.
+        if starting_cell_row in (8, 9, 10):
+            starting_cell_row = randint(1, 7)
+
+        scr = starting_cell_row
+
+        # remove used cells from the list
+        for _ in range(4):
+            field[scr][starting_cell_column] = 1
+            list_of_empty_cells.remove(str(scr) + '-' + str(starting_cell_column))
+            scr += 1
+        if (str(scr) + '-' + str(starting_cell_column)) in list_of_empty_cells:
+            list_of_empty_cells.remove(str(scr) + '-' + str(starting_cell_column))
+
+        scr = starting_cell_row - 1
+        if (str(scr) + '-' + str(starting_cell_column)) in list_of_empty_cells :
+            list_of_empty_cells.remove(str(scr) + '-' + str(starting_cell_column))
+
+        field[starting_cell_row-1][starting_cell_column] = 8
+        field[starting_cell_row+4][starting_cell_column] = 8
+
+        # removal of used cells from the list if the ship is adjacent to the borders of the field
+        if 1 < starting_cell_column < 10:
+            for column in [starting_cell_column - 1, starting_cell_column + 1]:
+                scr = starting_cell_row - 1
+                for _ in range(6):
+                    field[scr][column] = 8
+                    if (str(scr) + '-' + str(column)) in list_of_empty_cells:
+                        list_of_empty_cells.remove(str(scr) + '-' + str(column))
+                    scr += 1
+        elif starting_cell_column == 1:
+            scr = starting_cell_row - 1
+            for _ in range(6):
+                field[scr][starting_cell_column + 1] = 8
+                if (str(scr) + '-' + str(starting_cell_column + 1)) in list_of_empty_cells:
+                    list_of_empty_cells.remove(str(scr) + '-' + str(starting_cell_column + 1))
+                scr += 1
+        elif starting_cell_column == 10:
+            scr = starting_cell_row - 1
+            for _ in range(6):
+                field[scr][starting_cell_column - 1] = 8
+                if (str(scr) + '-' + str(starting_cell_column - 1)) in list_of_empty_cells:
+                    list_of_empty_cells.remove(str(scr) + '-' + str(starting_cell_column - 1))
+                scr += 1
+    return field, list_of_empty_cells
+
+#
+# field, list_of_empty_cells = Position_of_a_ship_with_4_slots()
+#
+# for i in range(12):
+#      print(field[i])
