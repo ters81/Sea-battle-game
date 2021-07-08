7303743e93e59087731dffb3b4ac4f14c33e671c
Serhii Tereshchuk <57225875+ters81@users.noreply.github.com>
2021-07-08 17:16:49 +0300

    Add files via upload

--- Position_of_a_ship_with_3_slots.py
+++ Position_of_a_ship_with_3_slots.py
@@ -61,121 +61,126 @@ def Position_of_a_ship_with_3_slots():
                 scc += 1
 
     # ================================================================================================================
+    while count_of_ship_3 < 2:
 
-    direction = choice(['horizontal', 'vertical'])
+        direction = choice(['horizontal', 'vertical'])
 
-    direction = 'horizontal'  # Проверка -----------------------------------------------------------------------------
+        direction = 'horizontal'  # Проверка --------------------------------------------------------------------------
 
-    starting_cell = choice(list_of_empty_cells).split('-')
+        starting_cell = choice(list_of_empty_cells).split('-')
 
-    starting_cell = '1-4'.split('-')  # ПРОВЕРКА -------------------------------------------------------------
+        # starting_cell = '1-4'.split('-')  # ПРОВЕРКА -------------------------------------------------------------
 
-    starting_cell_row = int(starting_cell[0])
-    starting_cell_column = int(starting_cell[1])
-
-    print(starting_cell_row, starting_cell_column, direction)
-
-    while count_of_ship_3 < 2 ////////////////////////////////////////////////////////////////////
-
-
-    if direction == 'horizontal':
-        if field[starting_cell_row].count(0) == 10:
-            if starting_cell_column in (9, 10):
-                starting_cell_column = randint(1, 8)
-            Ship_3_horizontal()
-
-        elif field[starting_cell_row].count(0) == 5 and field[starting_cell_row][10] == 0:
-            starting_cell_column = randint(6, 8)
-            Ship_3_horizontal()
-
-        elif field[starting_cell_row].count(0) == 5 and field[starting_cell_row][1] == 0:
-            starting_cell_column = randint(1, 3)
-            Ship_3_horizontal()
-
-        elif field[starting_cell_row].count(0) == 4 and field[starting_cell_row][7] == 0 and field[starting_cell_row][
-            10] == 0:
-            starting_cell_column = randint(7, 8)
-            Ship_3_horizontal()
-
-        elif field[starting_cell_row].count(0) == 4 and field[starting_cell_row][1] == 0 and field[starting_cell_row][
-            4] == 0:
-            starting_cell_column = randint(1, 2)
-            Ship_3_horizontal()
-
-        elif field[starting_cell_row].count(0) == 4 and field[starting_cell_row][1] == 0 and field[starting_cell_row][
-            8] == 0:
-            starting_cell_column = 8
-            Ship_3_horizontal()
-
-        elif field[starting_cell_row].count(0) == 4 and field[starting_cell_row][3] == 0 and field[starting_cell_row][
-            10] == 0:
-            starting_cell_column = 1
-            Ship_3_horizontal()
-
-        elif field[starting_cell_row].count(0) == 7:
-            pass
-
-        elif field[starting_cell_row].count(0) == 8:
-            pass
-        else:  # когда горизонтадбный 4-х палубный посредине (2 пустые с каждой стороны)
-            pass
+        starting_cell_row = int(starting_cell[0])
+        starting_cell_column = int(starting_cell[1])
 
         print(starting_cell_row, starting_cell_column, direction)
 
-    else:    # if direction == 'vertical'
-        count_of_0 = 0
+        if direction == 'horizontal':
+            if field[starting_cell_row].count(0) == 10:
+                if starting_cell_column in (9, 10):
+                    starting_cell_column = randint(1, 8)
+                Ship_3_horizontal()
 
-        for i in range(1, 11):
-            if field[i][starting_cell_column] == 0:
-                count_of_0 += 1
+            elif field[starting_cell_row].count(0) == 5 and field[starting_cell_row][10] == 0:
+                starting_cell_column = randint(6, 8)
+                Ship_3_horizontal()
 
-        if count_of_0 == 10:
-            # If the initial position of the ship will cause it to go out of the field.
-            if starting_cell_row in (9, 10):
-                starting_cell_row = randint(1, 8)
+            elif field[starting_cell_row].count(0) == 5 and field[starting_cell_row][1] == 0:
+                starting_cell_column = randint(1, 3)
+                Ship_3_horizontal()
+
+            elif field[starting_cell_row].count(0) == 4 and field[starting_cell_row][7] == 0 and field[starting_cell_row][10] == 0:
+                starting_cell_column = randint(7, 8)
+                Ship_3_horizontal()
+
+            elif field[starting_cell_row].count(0) == 4 and field[starting_cell_row][1] == 0 and field[starting_cell_row][4] == 0:
+                starting_cell_column = randint(1, 2)
+                Ship_3_horizontal()
+
+            elif field[starting_cell_row].count(0) == 4 and field[starting_cell_row][1] == 0 and field[starting_cell_row][8] == 0:
+                starting_cell_column = 8
+                Ship_3_horizontal()
+
+            elif field[starting_cell_row].count(0) == 4 and field[starting_cell_row][3] == 0 and field[starting_cell_row][10] == 0:
+                starting_cell_column = 1
+                Ship_3_horizontal()
+
+            elif field[starting_cell_row].count(0) == 3 and field[starting_cell_row][1] == 0 and field[starting_cell_row][3] == 0:
+                starting_cell_column = 1
+                Ship_3_horizontal()
+
+            elif field[starting_cell_row].count(0) == 3 and field[starting_cell_row][7] == 0 and field[starting_cell_row][10] == 0:
+                starting_cell_column = 7
+                Ship_3_horizontal()
+
+            elif field[starting_cell_row].count(0) == 7 and (0 not in field[starting_cell_row][1:4] or
+                                                             0 not in field[starting_cell_row][2:5] or
+                                                             0 not in field[starting_cell_row][3:6] 
+            ):
+                pass
+
+            elif field[starting_cell_row].count(0) == 8:
+                pass
+            else:  # когда горизонтадбный 4-х палубный посредине (2 пустые с каждой стороны)
+                pass
 
             print(starting_cell_row, starting_cell_column, direction)
 
-            scr = starting_cell_row
+        else:    # if direction == 'vertical'
+            count_of_0 = 0
 
-            # remove used cells from the list
-            for _ in range(3):
-                field[scr][starting_cell_column] = 1
-                list_of_empty_cells.remove(str(scr) + '-' + str(starting_cell_column))
-                scr += 1
-            if (str(scr) + '-' + str(starting_cell_column)) in list_of_empty_cells:
-                list_of_empty_cells.remove(str(scr) + '-' + str(starting_cell_column))
+            for i in range(1, 11):
+                if field[i][starting_cell_column] == 0:
+                    count_of_0 += 1
 
-            scr = starting_cell_row - 1
-            if (str(scr) + '-' + str(starting_cell_column)) in list_of_empty_cells:
-                list_of_empty_cells.remove(str(scr) + '-' + str(starting_cell_column))
+            if count_of_0 == 10:
+                # If the initial position of the ship will cause it to go out of the field.
+                if starting_cell_row in (9, 10):
+                    starting_cell_row = randint(1, 8)
 
-            field[starting_cell_row - 1][starting_cell_column] = 8
-            field[starting_cell_row + 3][starting_cell_column] = 8
+                print(starting_cell_row, starting_cell_column, direction)
 
-            # removal of used cells from the list if the ship is adjacent to the borders of the field
-            if 1 < starting_cell_column < 10:
-                for column in [starting_cell_column - 1, starting_cell_column + 1]:
+                scr = starting_cell_row
+
+                # remove used cells from the list
+                for _ in range(3):
+                    field[scr][starting_cell_column] = 1
+                    list_of_empty_cells.remove(str(scr) + '-' + str(starting_cell_column))
+                    scr += 1
+                if (str(scr) + '-' + str(starting_cell_column)) in list_of_empty_cells:
+                    list_of_empty_cells.remove(str(scr) + '-' + str(starting_cell_column))
+
+                scr = starting_cell_row - 1
+                if (str(scr) + '-' + str(starting_cell_column)) in list_of_empty_cells:
+                    list_of_empty_cells.remove(str(scr) + '-' + str(starting_cell_column))
+
+                field[starting_cell_row - 1][starting_cell_column] = 8
+                field[starting_cell_row + 3][starting_cell_column] = 8
+
+                # removal of used cells from the list if the ship is adjacent to the borders of the field
+                if 1 < starting_cell_column < 10:
+                    for column in [starting_cell_column - 1, starting_cell_column + 1]:
+                        scr = starting_cell_row - 1
+                        for _ in range(5):
+                            field[scr][column] = 8
+                            if (str(scr) + '-' + str(column)) in list_of_empty_cells:
+                                list_of_empty_cells.remove(str(scr) + '-' + str(column))
+                            scr += 1
+                elif starting_cell_column == 1:
                     scr = starting_cell_row - 1
                     for _ in range(5):
-                        field[scr][column] = 8
-                        if (str(scr) + '-' + str(column)) in list_of_empty_cells:
-                            list_of_empty_cells.remove(str(scr) + '-' + str(column))
+                        field[scr][starting_cell_column + 1] = 8
+                        if (str(scr) + '-' + str(starting_cell_column + 1)) in list_of_empty_cells:
+                            list_of_empty_cells.remove(str(scr) + '-' + str(starting_cell_column + 1))
                         scr += 1
-            elif starting_cell_column == 1:
-                scr = starting_cell_row - 1
-                for _ in range(5):
-                    field[scr][starting_cell_column + 1] = 8
-                    if (str(scr) + '-' + str(starting_cell_column + 1)) in list_of_empty_cells:
-                        list_of_empty_cells.remove(str(scr) + '-' + str(starting_cell_column + 1))
-                    scr += 1
-            elif starting_cell_column == 10:
-                scr = starting_cell_row - 1
-                for _ in range(5):
-                    field[scr][starting_cell_column - 1] = 8
-                    if (str(scr) + '-' + str(starting_cell_column - 1)) in list_of_empty_cells:
-                        list_of_empty_cells.remove(str(scr) + '-' + str(starting_cell_column - 1))
-                    scr += 1
+                elif starting_cell_column == 10:
+                    scr = starting_cell_row - 1
+                    for _ in range(5):
+                        field[scr][starting_cell_column - 1] = 8
+                        if (str(scr) + '-' + str(starting_cell_column - 1)) in list_of_empty_cells:
+                            list_of_empty_cells.remove(str(scr) + '-' + str(starting_cell_column - 1))
+                        scr += 1
 
     return list_of_empty_cells
 
